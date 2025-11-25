6c3bc4870812556da70e43403bdde26b19bb28d5
katsunny <carolopes2002@gmail.com>
2025-11-25 11:56:50 -0300

    add setlocale

--- programa.c
+++ programa.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <locale.h>
 
 int main(){
-    printf("*************************************\n");
-    printf("* Bem-vindo ao jogo da adivinhacao! *\n");
-    printf("*************************************\n");
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("\n\n");
+    printf("  __| |____________________________________________| |__  \n");
+    printf(" (__   ____________________________________________   __) \n");
+    printf("    | |                                            | |    \n");
+    printf("    | |           SEJA MUITO BEM VINDO AO          | |    \n");
+    printf("    | |            JOGO DA ADIVINHACAO!!           | |    \n");
+    printf("    | |                                            | |    \n");
+    printf("    | |                 by: calpsz                 | |    \n");
+    printf("    | |                                            | |    \n");
+    printf("  __| |____________________________________________| |__  \n");
+    printf(" (__   ____________________________________________   __) \n");
+    printf("    | |                                            | |    \n");
+    printf("\n\n");
+
+    int nivel;
+    printf("Qual nivel de dificuldade quer jogar?\n");
+    printf("[1] Facil\n[2] Medio\n[3] Dificil\n\n");
+    printf("Escolha: ");
+    scanf("%d", &nivel);
 
     int segundos = time(0);
     srand(segundos);
@@ -18,11 +37,31 @@ int main(){
     int chute;
     int tentativas = 1;
 
+    int acertou = 0;
+
+    //max pontos determinados pro jogo
     double pontos = 1000;
 
+    
 
-    while(1){
-        printf("tentativa %d\n", tentativas);
+    int numerodetentativas;
+    switch (nivel){
+    case 1:
+        numerodetentativas = 20;
+        break;
+
+    case 2:
+        numerodetentativas = 15;
+        break;
+    
+    default:
+        numerodetentativas = 6;
+        break;
+    }
+
+
+    for(int i = 1; i <= numerodetentativas; i++){
+        printf("tentativa %d de %d\n", tentativas, numerodetentativas);
 
         printf("Qual o seu chute? ");
         scanf("%d", &chute);
@@ -34,12 +73,11 @@ int main(){
         }
 
         //basicamente um boolean
-        int acertou = (chute == numerosecreto);
+        acertou = (chute == numerosecreto);
         int menor = (chute < numerosecreto);
 
 
         if(acertou){
-            printf("Vc acertou!!! PARABENSSS!!! UHUUULL\n\n");
             break;
         }
         else if (menor){
@@ -57,7 +95,15 @@ int main(){
     
     }
     printf("Fim de jogo!\n");
-    printf("Numero de tentativas: %d\n", tentativas);
-    printf("Total de pontos: %.2f", pontos);
+
+    if(acertou){
+        printf("Vc acertou!!! PARABENSSS!!! UHUUULL ദ്ദി •⩊• )\n");
+        printf("Numero de tentativas: %d\n", tentativas);
+        printf("Total de pontos: %.2f\n", pontos);
+    } else {
+        printf("Voce perdeu D:\nTente de novo!\n");
+    }
+
+    
     
 }
